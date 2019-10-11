4a01c9f2c82eb436cbc92ceda41d6941d27f0e66
Denvinn Magsino <denvinnmagsino@denvinns-air.home>
2019-10-10 19:17:29 -0700

    finished MVP

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,7 +1,6 @@
 import React from "react";
 import axios from "axios";
 import MovieCard from "./MovieCard";
-import UpdateMovie from "./UpdateMovie";
 
 export default class Movie extends React.Component {
   constructor(props) {
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import axios from "axios";
 
 const MovieCard = props => {
 
   const { title, director, metascore, stars, id } = props.movie;
+
+  const deleteMovie = e => {
+    
+    axios
+      .delete(`http://localhost:5000/api/movies/${id}`)
+      .then(res => props.history.push(`/`))
+      .catch(err => console.log(err))
+  }
   return (
     <div className="movie-card">
       <h2>{title}</h2>
@@ -21,6 +30,9 @@ const MovieCard = props => {
       ))}
       <button onClick={() => props.history.push(`/updatemovie/${id}`)}>
         Edit Movie
+      </button>
+      <button onClick={deleteMovie}>
+        Delete Movie
       </button>
     </div>
   );
